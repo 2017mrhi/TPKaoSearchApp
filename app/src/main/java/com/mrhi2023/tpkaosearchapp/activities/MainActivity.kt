2753bb2323ed07cb2b27c36bfc2a0bb6ab7a1d77
2017mrhi <2017mrhi@gmail.com>
2023-03-28 10:28:24 +0900

    package 분리

--- app/src/main/java/com/mrhi2023/tpkaosearchapp/activities/MainActivity.kt
+++ app/src/main/java/com/mrhi2023/tpkaosearchapp/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.mrhi2023.tpkaosearchapp
+package com.mrhi2023.tpkaosearchapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mrhi2023.tpkaosearchapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
