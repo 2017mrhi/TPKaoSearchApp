1c088967d5cf77f9eab00484dbbfbe4d23b801b8
2017mrhi <2017mrhi@gmail.com>
2023-03-29 10:58:38 +0900

    LoginActivity UI 버튼 리스너 처리

--- app/src/main/java/com/mrhi2023/tpkaosearchapp/activities/LoginActivity.kt
+++ app/src/main/java/com/mrhi2023/tpkaosearchapp/activities/LoginActivity.kt
@@ -1,12 +1,54 @@
 package com.mrhi2023.tpkaosearchapp.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mrhi2023.tpkaosearchapp.R
+import com.mrhi2023.tpkaosearchapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    val binding:ActivityLoginBinding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+
+        //둘러보기 버튼 클릭으로 로그인없이 Main 화면으로 이동
+        binding.tvGo.setOnClickListener {
+            startActivity( Intent(this, MainActivity::class.java) )
+            finish()
+        }
+
+        //회원가입 버튼 클릭
+        binding.tvSignup.setOnClickListener {
+            //회원가입화면으로 전환
+            startActivity(Intent(this, SignupActivity::class.java))
+        }
+
+        //이메일 로그인 버튼 클릭
+        binding.layoutEmail.setOnClickListener {
+            //이메일 로그인 화면으로 전환
+            startActivity(Intent(this, EmailSigninActivity::class.java))
+        }
+
+        //간편로그인 버튼들 클릭
+        binding.ivLoginKakao.setOnClickListener { clickedLoginKakao() }
+        binding.ivLoginGoogle.setOnClickListener { clickedLoginGoogle() }
+        binding.ivLoginNaver.setOnClickListener { clickedLoginNaver() }
+
+    }//onCreate method..
+
+    private fun clickedLoginKakao(){
+
     }
+
+    private fun clickedLoginGoogle(){
+
+    }
+
+    private fun clickedLoginNaver(){
+
+    }
+
 }
---
+++ app/src/main/java/com/mrhi2023/tpkaosearchapp/activities/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.mrhi2023.tpkaosearchapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mrhi2023.tpkaosearchapp.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
